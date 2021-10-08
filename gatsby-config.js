5f205cf1d6bd50bea5d60c6a4644a92eaa5c45f1
Bramdelfos <34161968+Bramdelfos@users.noreply.github.com>
2021-10-08 13:17:21 +0200

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,34 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Arlo Portfolio React Template`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: "http://localhost:8000/",
+    getform_url: "https://getform.io/f/7a6695a7-c8e3-442c-bc2f-d46d3b9a535e",
   },
+
+  mapping: {
+    "MarkdownRemark.frontmatter.author": `AuthorsJson.name`,
+  },
+
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images/`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -24,13 +36,39 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    
+    {
+        resolve: "gatsby-plugin-anchor-links",
+        options: {
+          offset: -100
+        }
+    },
+
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1920
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-json`,
+    
+    
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,191 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
 
-// You can delete this file if you're not using it
+const { slugify } = require('./src/utils/utilityFunctions');
+const path = require('path');
+const _ = require('lodash');
+
+
+exports.onCreateNode = ({node , actions}) => {
+    const { createNodeField } = actions;
+    if (node.internal.type === 'MarkdownRemark') {
+        const slugFromTitle = slugify(node.frontmatter.title)
+        createNodeField({
+            node, 
+            name: 'slug',
+            value: slugFromTitle,
+        });
+
+        if (Object.prototype.hasOwnProperty.call(node.frontmatter, "author")) {
+            createNodeField({
+              node,
+              name: "authorId",
+              value: slugify(node.frontmatter.author)
+            });
+        }
+    }
+    
+    if(node.internal.type === 'AuthorsJson'){
+        createNodeField({
+            node,
+            name: "authorId",
+            value: slugify(node.name)
+        });
+    }
+
+}
+
+exports.createPages = ({actions, graphql}) => {
+    const { createPage } = actions;
+    const templates =  {
+        projectDetails: path.resolve('src/template/project-details.js'),
+        blogDetails: path.resolve('src/template/blog-details.js'),
+        categoryPost: path.resolve('src/template/category-post.js'),
+        tagPost: path.resolve('src/template/tag-template.js'),
+        authorPage: path.resolve('src/template/archive.js'),
+    }
+
+    return graphql(`
+        {
+            allProjectJson {
+                edges {
+                    node {
+                        id
+                    }
+                }
+            }
+
+
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                            authorId
+                        }
+                        frontmatter {
+                            author {
+                                name
+                            }
+                            tags
+                            category
+                        }
+                    }
+                }
+            }
+            
+
+        }
+    `).then( res => {
+        if (res.errors) return Promise.reject(res.errors)
+        const project = res.data.allProjectJson.edges
+        const posts = res.data.allMarkdownRemark.edges
+
+         // Create Project Page
+         project.forEach(({ node }) => {
+            createPage({
+                // path: node.fields.slug,
+                path: `project/${slugify(node.id)}`,
+                component: templates.projectDetails,
+                context: {
+                    id: node.id
+                }
+            })
+        })
+
+        // Create Single Blog Page 
+        posts.forEach(({ node }) => {
+            createPage({
+                path: `${slugify(node.fields.slug)}`,
+                component: templates.blogDetails,
+                context: {
+                    slug: node.fields.slug
+                }
+            })
+        })
+
+        // Create Single Blog Page 
+
+        // Start Category Area 
+
+        // For get All Categiry Pages 
+        let categories = []
+        _.each(posts , edge => {
+            if (_.get(edge , 'node.frontmatter.category')) {
+                categories = categories.concat(edge.node.frontmatter.category)
+            }
+        })
+
+        // [design , code]
+        let categoryPostCounts = {}
+        categories.forEach( category => {
+            categoryPostCounts[category] = (categoryPostCounts[category] || 0) + 1
+        })
+        categories = _.uniq(categories)
+
+       
+        // Create Tag Posts Pages for indivedual Tag page
+        categories.forEach(category => {
+            createPage({
+                path: `/category/${slugify(category)}`,
+                component: templates.categoryPost,
+                context: {
+                    category 
+                }
+            })
+        })
+        // End Category Area
+
+
+
+        // Start Tags Pages 
+        let tags = []
+        _.each(posts , edge => {
+            if (_.get(edge , 'node.frontmatter.tags')) {
+                tags = tags.concat(edge.node.frontmatter.tags)
+            }
+        })
+        // Create Tag Posts Pages for indivedual Tag page
+        tags.forEach(tag => {
+            createPage({
+                path: `/tag/${slugify(tag)}`,
+                component: templates.tagPost,
+                context: {
+                    tag
+                }
+            })
+        })
+        // End Category Area
+
+
+
+        // Start Create Authors Page 
+        let authors = []
+        _.each(posts, edge => {
+            if(_.get(edge, 'node.fields.authorId')){
+                authors = authors.concat(edge.node.fields.authorId)
+            }
+        })
+        authors = _.uniq(authors)
+        authors.forEach(author => {
+            createPage({
+                path: `/author/${slugify(author)}`,
+                component: templates.authorPage,
+                context: {
+                    author
+                }
+            })
+        })
+        // End Create Authors Page
+
+
+
+
+
+    })
+
+}
+
+
+
+
+
+
