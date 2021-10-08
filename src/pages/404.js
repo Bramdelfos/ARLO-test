8872ffafc72fde9f387c6e52534a6d17959bd019
Bramdelfos <34161968+Bramdelfos@users.noreply.github.com>
2021-10-08 14:26:47 +0200

    Add files via upload

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,26 @@
-import * as React from "react"
-
+import React from "react";
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404: Not found" />
+    <div className="error-page-wrapper">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="inner">
+                        <h1 className="theme-color">404</h1>
+                        <h2>Page Not Found</h2>
+                        <p>Sorry, but the page you are looking for was moved, removed, renamed or might never existed...</p>
+                        <a className="rn-button" href="/">Go Back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   </Layout>
 )
-
 export default NotFoundPage
+
+
