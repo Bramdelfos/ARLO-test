da2eb6f37258c96ddfea33ad765e022ed6b942a7
Bramdelfos <34161968+Bramdelfos@users.noreply.github.com>
2021-10-14 13:48:39 +0200

    Update thumbnail.stc.js

--- src/components/post-media/thumbnail/thumbnail.stc.js
+++ src/components/post-media/thumbnail/thumbnail.stc.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const BlogThumb = styled.figure `
-    height: 50%;
-    width: 50%;
+    height: 100%;
+    width: 100%;
     position: relative;
     a {
         display: block;
