a1e72976a33db6f69a56f204ec6ea9de6ae9466e
Bramdelfos <34161968+Bramdelfos@users.noreply.github.com>
2021-11-11 15:28:52 +0100

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -17,7 +17,7 @@ const Header = () => {
                     path
                 }
             },
-            file(relativePath: {eq: "images/logo/NEWNETNINJA.png"}) {
+            file(relativePath: {eq: "images/logo/NNN-white-small.png"}) {
                 childImageSharp {
                   fixed {
                     ...GatsbyImageSharpFixed
