7090a18a7bdbdbbca97de4f66fa862b5411dd787
Bramdelfos <34161968+Bramdelfos@users.noreply.github.com>
2021-11-17 14:42:20 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Arlo Portfolio React Template`,
+    title: `Demystifying The Metaverse`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: "http://localhost:8000/",
